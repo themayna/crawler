3f51abb3d8c6b24559c4bc187cf60b4aad577370
mainericiangel <P@rola1234>
2017-02-10 18:53:34 +0200

    refactorizare consumeri sites + category

---
+++ src/AppBundle/Command/SiteConsumerCommand.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mainericiangel
+ * Date: 2/1/17
+ * Time: 12:11 AM
+ */
+namespace AppBundle\Command;
+
+use AppBundle\Services\Consumers\SitesConsumer;
+
+class SiteConsumerCommand extends BaseConsumerWrapper
+{
+
+    protected function getConsumerName()
+    {
+        return 'sites';
+    }
+
+    protected function getConsummerCommandDescription()
+    {
+        return 'Gets sites data and publises categories on queue';
+    }
+
+    protected function getMessage($msg)
+    {
+        /** @var SitesConsumer $serice */
+        $serice = $this->getContainer()->get(SitesConsumer::serviceName);
+        $serice->saveCategories($msg);
+    }
+
+}
